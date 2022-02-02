056c5668c74ff2e90d44b6be9bd07eb4ae3ba3b8
dydx <dydx2000.py@gmail.com>
2022-02-02 15:50:55 +0800

    add 04

---
+++ Pointer_study/20220129/pointer_arr04.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    int d = 40;
+    int *arr[4] = {&a, &b, &c, &d};
+    int i = 0;
+    for (i = 0; i < 4; i++)
+    {
+        printf("%d ", *(arr[i]));
+    }
+    return 0;
+}
